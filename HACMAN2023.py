2bb6f9c3a92a4e5004200f83e56f18191fa3286e
PrathamGBhat <149788500+PrathamGBhat@users.noreply.github.com>
2023-11-04 10:43:44 +0530

    Add files via upload

---
+++ HACMAN2023.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import *
+window=tk.Tk()
+frame=Frame(width=1440,height=1024,background='#475682')
+my_canvas=Canvas(width=1440,height=1024,background='#475682',Text='')
+canvas.itemconfig
+window.mainloop()
